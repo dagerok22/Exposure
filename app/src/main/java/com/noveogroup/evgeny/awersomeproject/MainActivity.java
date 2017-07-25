33620f4c49992d34f1b57efd0553ebfaeef39b4a
BelovEvgeny <zheka_bel_14@yahoo.com>
2017-07-25 11:55:53 +0700

    delited .idea

--- app/src/main/java/com/noveogroup/evgeny/awersomeproject/MainActivity.java
+++ app/src/main/java/com/noveogroup/evgeny/awersomeproject/MainActivity.java
@@ -1,20 +1,16 @@
 package com.noveogroup.evgeny.awersomeproject;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 import clarifai2.api.ClarifaiBuilder;
 import clarifai2.api.ClarifaiClient;
+import clarifai2.api.request.ClarifaiRequest;
 import clarifai2.dto.input.ClarifaiImage;
 import clarifai2.dto.input.ClarifaiInput;
 import clarifai2.dto.model.output.ClarifaiOutput;
@@ -30,23 +26,34 @@ public class MainActivity extends AppCompatActivity {
         final ClarifaiClient client = new ClarifaiBuilder("f7bcefcc6cbf45219549bc97714c8604").buildSync();
         //ClarifaiClient client = new Cla
         //new ClarifaiBuilder("{api-key}").buildSync().registerAsDefaultInstance();
-        AsyncTask<Integer, Integer, List<ClarifaiOutput<Concept>>> asyncTask = new AsyncTask<Integer, Integer, List<ClarifaiOutput<Concept>>>() {
-            @Override
-            protected List<ClarifaiOutput<Concept>> doInBackground(Integer... params) {
-                Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.dogs);
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
-                byte[] byteArray = stream.toByteArray();
-                return client.getDefaultModels().generalModel() // You can also do Clarifai.getModelByID("id") to get custom models
-                        .predict()
-                        .withInputs(
-                                //ClarifaiInput.forImage(ClarifaiImage.of("https://samples.clarifai.com/metro-north.jpg"))
-                                ClarifaiInput.forImage(ClarifaiImage.of(byteArray))
-                        )
-                        .executeSync() // optionally, pass a ClarifaiClient parameter to override the default client instance with another one
-                        .get();
-            }
-        };
+
+
+//                Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.dogs);
+//                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//                byte[] byteArray = stream.toByteArray();
+        client.getDefaultModels().generalModel() // You can also do Clarifai.getModelByID("id") to get custom models
+                .predict()
+                .withInputs(
+                        ClarifaiInput.forImage(ClarifaiImage.of("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8cBb59-SULE4c5JKXE85Jw8hwzrx1HCi5jyqVvFqGyxcXo20AYA"))
+                        //ClarifaiInput.forImage(ClarifaiImage.of(byteArray))
+                )
+                .executeAsync(new ClarifaiRequest.Callback<List<ClarifaiOutput<Concept>>>() {
+                    @Override
+                    public void onClarifaiResponseSuccess(List<ClarifaiOutput<Concept>> clarifaiOutputs) {
+                        predictionResults = clarifaiOutputs;
+                    }
+
+                    @Override
+                    public void onClarifaiResponseUnsuccessful(int errorCode) {
+
+                    }
+
+                    @Override
+                    public void onClarifaiResponseNetworkError(IOException e) {
+
+                    }
+                });
 
 //        try {
 //            predictionResults = asyncTask.execute().get();
