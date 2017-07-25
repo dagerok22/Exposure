b371c635f8e400db3db8ac5a7aeb7f1f58b67066
suslov.sergey <sergey.suslov.nsk@gmail.com>
2017-07-25 14:52:39 +0700

    added location, maps dependencies, db package

---
+++ app/src/main/java/com/noveogroup/evgeny/awersomeproject/db/model/Task.java
@@ -0,0 +1,15 @@
+package com.noveogroup.evgeny.awersomeproject.db.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by admin on 25.07.2017.
+ */
+
+public class Task {
+    private int id;
+    private String name;
+    private String[] tags;
+    private String imageUrl;
+    private LatLng point;
+}
