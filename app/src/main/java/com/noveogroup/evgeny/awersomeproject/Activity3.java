c31a74a74d5a0d96aa5e81abdae26d65197554a9
Belov Evgeny <zheka_bel_14@yahoo.com>
2017-07-25 16:40:40 +0700

    work only emulator

--- app/src/main/java/com/noveogroup/evgeny/awersomeproject/Activity3.java
+++ app/src/main/java/com/noveogroup/evgeny/awersomeproject/Activity3.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
@@ -11,22 +12,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiImage;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
 
 public class Activity3 extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 2;
+
+    static final int REQUEST_TAKE_PHOTO = 225;
+    static final String TAG = "MainActivity";
     @BindView(R.id.imageView)
     public ImageView imageView;
     String mCurrentPhotoPath;
+    List<ClarifaiOutput<Concept>> predictionResults;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,18 +81,55 @@ public class Activity3 extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            File imgFile = new File(mCurrentPhotoPath);
+            //File imgFile = new File(mCurrentPhotoPath);
+            Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
 
-            if (imgFile.exists()) {
+            //Bitmap d = new BitmapDrawable(ctx.getResources() , w.photo.getAbsolutePath()).getBitmap();
+            int nh = (int) ( bitmap.getHeight() * (1024.0 / bitmap.getWidth()) );
+            Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 1024, nh, true);
 
-                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-                imageView.setImageBitmap(myBitmap);
-
-            }
+//            if (imgFile.exists()) {
+//                try {
+//                    Bitmap myBitmap = Glide.with(this).asBitmap().load(imgFile).into(340, 340).get();
+//                    imageView.setImageBitmap(myBitmap);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                } catch (ExecutionException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+            imageView.setImageBitmap(scaled);
+//            Glide.with(this).load(imgFile)
+//                    .into(imageView);
+            sendToCloud();
 
         }
     }
 
+    void sendToCloud() {
+        AsyncTask<Void,Void, List<ClarifaiOutput<Concept>>> asyncTask = new AsyncTask<Void, Void, List<ClarifaiOutput<Concept>>>() {
+            @Override
+            protected List<ClarifaiOutput<Concept>> doInBackground(Void... params) {
+                Log.d("asd","doInbackground");
+                final ClarifaiClient client = new ClarifaiBuilder("f7bcefcc6cbf45219549bc97714c8604").buildSync();
+
+               return client.getDefaultModels().generalModel() // You can also do Clarifai.getModelByID("id") to get custom models
+                        .predict()
+                        .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(new File(mCurrentPhotoPath))))
+                        .executeSync()
+                        .get();
+            }
+
+            @Override
+            protected void onPostExecute(List<ClarifaiOutput<Concept>> clarifaiOutputs) {
+                predictionResults = clarifaiOutputs;
+                Log.d("asd","onPost");
+            }
+        };
+        asyncTask.execute((Void[]) null);
+
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
--- app/src/main/java/com/noveogroup/evgeny/awersomeproject/MainActivity.java
+++ app/src/main/java/com/noveogroup/evgeny/awersomeproject/MainActivity.java
@@ -4,56 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.List;
-
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import clarifai2.dto.model.output.ClarifaiOutput;
-import clarifai2.dto.prediction.Concept;
 
 public class MainActivity extends AppCompatActivity {
-    static final String TAG = "MainActivity";
-    List<ClarifaiOutput<Concept>> predictionResults;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-//        final ClarifaiClient client = new ClarifaiBuilder("f7bcefcc6cbf45219549bc97714c8604").buildSync();
-//
-//        client.getDefaultModels().generalModel() // You can also do Clarifai.getModelByID("id") to get custom models
-//                .predict()
-//                .withInputs(
-//                        //ClarifaiInput.forImage(array)
-//                        ClarifaiInput.forImage(ClarifaiImage.of("http://cdn.fishki.net/upload/post/2017/06/29/2324753/smeshnye-kartinki-s-nadpisjami-0.jpg"))
-//                )
-//                .executeAsync(new ClarifaiRequest.Callback<List<ClarifaiOutput<Concept>>>() {
-//                    @Override
-//                    public void onClarifaiResponseSuccess(List<ClarifaiOutput<Concept>> clarifaiOutputs) {
-//                        predictionResults = clarifaiOutputs;
-//                        Log.d(TAG,"ready");
-//                    }
-//
-//                    @Override
-//                    public void onClarifaiResponseUnsuccessful(int errorCode) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onClarifaiResponseNetworkError(IOException e) {
-//
-//                    }
-//                });
-
         System.out.println("asd");
     }
 
-    //    public void onClick(View view) {
-//        this.getFilesDir().toString();
-//        TextView textView = (TextView) findViewById(R.id.text_view);
-//        textView.setText(predictionResults.get(0).data().get(0).toString());
-//    }
+
     @OnClick(R.id.on_screen3_button)
     public void onScreen3() {
         Intent intent = new Intent(MainActivity.this, Activity3.class);
